945f0a8592e04a3a4f0396c10a723c8f7b18c34f
pattlebass <49322676+pattlebass@users.noreply.github.com>
2020-02-27 17:04:15 +0200

    Add files

--- Road Safety/player.gd
+++ Road Safety/player.gd
@@ -1,22 +1,37 @@
 extends KinematicBody
 
 onready var camera = get_parent().get_node("Camera")
+var car_rigid_scene = preload("res://car_rigid.tscn")
 var velocity = Vector3()
-var speed = 200
+var speed = 500
+var turn = 200
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	#Teleport
+	print(translation.x)
+	if translation.x >= 40:
+		translation.x = -48
 	#Camera
-	camera.translation.x = translation.x - 2
+	if speed != 0:
+		camera.translation.x = translation.x - 1.5
 	
 	#Controls
 	velocity.z = 0
-	velocity.x = 1000
+	velocity.x = speed
 	if Input.is_action_pressed("left"):
-		velocity.z = -speed
+		velocity.z = -turn
 	if Input.is_action_pressed("right"):
-		velocity.z = speed
+		velocity.z = turn
+	if Input.is_action_just_pressed("ui_accept"):
+		var car_rigid = car_rigid_scene.instance()
+		translation = car_rigid.translation
+		get_parent().add_child(car_rigid)
+		#car_rigid.apply_central_impulse(Vector3(200, 0, 0))
+		speed = 0
+		visible = false
 		
 	move_and_slide(velocity * delta)
+
--- Road Safety/popups/popups.gd
+++ Road Safety/popups/popups.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var frequency = 9999
+var frequency = 3
 var notif1_scene = preload("res://popups/notification1.tscn")
 var call_scene = preload("res://popups/call.tscn")
 var popups = {0:notif1_scene, 1:call_scene}
@@ -14,7 +14,6 @@ func _process(delta):
 
 
 func _on_Timer_timeout():
-	#print(randi() % popups.size())
 	var current_popup = popups.get(randi() % popups.size()).instance()
 	current_popup.position = Vector2(randi() % 500, randi() % 300)
 	add_child(current_popup)
